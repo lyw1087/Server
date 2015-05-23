f83241f3da20bb7374fe32bcc7f955faec4497ed
lyw1087 <rkaahs2000@naver.com>
2015-05-23 16:34:30 +0900

    in

--- routes/contents.js
+++ routes/contents.js
@@ -1,36 +1,41 @@
 var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
-var connection = mysql.createConnection({
-'host' : '', 'user' : '', 'password' : '', 'database' : '',
+
+var conn = mysql.createConnection({
+	user: 'junghan',
+	host: 'sopt.cwzn9rhglsgx.ap-northeast-1.rds.amazonaws.com',
+	password: 'sopt-junghan',
+	database: 'sopt'
 });
+
 router.get('/:content_id', function(req, res, next) {
-connection.query('select * from board where id=?;',
-[req.params.content_id], function (error, cursor) {
-if (cursor.length > 0)
-res.json(cursor[0]);
-else
-res.status(503).json({ result : false, reason : "Cannot find selected article" });
+	connection.query('select * from board where id=?;', [req.params.content_id], 
+		function (error, cursor) {
+			if (cursor.length > 0) res.json(cursor[0]);
+			else res.status(503).json({ result : false, reason : "Cannot find selected article" });
+		}
+	); 
 });
-});
+
 router.post('/', function(req, res, next) {
-connection.query('insert into board(title, content) values (?, ?);',
-[req.body.title, req.body.content], function (error, info) {
-if (error == null) {
-connection.query('select * from board where id=?;',
-[info.insertId], function (error, cursor) {
-if (cursor.length > 0) {
-res.json({
-result : true, id : cursor[0].id, title : cursor[0].title,
-    timestamp : cursor[0].timestamp,
+	connection.query('insert into board(title, content) values (?, ?);', [req.body.title, req.body.content], 
+		function (error, info) {
+			if (error == null) {
+				connection.query('select * from board where id=?;', [info.insertId], 
+					function (error, cursor) {
+						if (cursor.length > 0) { 
+							res.json({ result : true, id : cursor[0].id, 
+								title : cursor[0].title,timestamp : cursor[0].timestamp, });
+						} 
+						else
+							res.status(503).json({ result : false, reason : "Cannot post article" }); 
+					}
+				);
+			}
+			else { res.status(503).json(error); }
+		}
+	);
 });
-}
-else
-res.status(503).json({ result : false, reason : "Cannot post article" });
-});
-}
-else
-res.status(503).json(error);
-});
-});
+
 module.exports = router;
